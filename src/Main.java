b5e0605628242a780294c730d94a699aefcb10be
Chief Master <421401388@qq.com>
2019-04-17 17:58:32 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
+import userInterface.GraphicalUserInterface;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            System.setProperty("sun.java2d.noddraw", "true");
+            UIManager.put("RootPane.setupButtonVisible", false);
+            BeautyEyeLNFHelper.translucencyAtFrameInactive = false;
+            BeautyEyeLNFHelper.frameBorderStyle = BeautyEyeLNFHelper.FrameBorderStyle.osLookAndFeelDecorated;
+            BeautyEyeLNFHelper.launchBeautyEyeLNF();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        GraphicalUserInterface graphicalUserInterface = new GraphicalUserInterface();
+        graphicalUserInterface.setVisible(true);
+    }
+
+}
